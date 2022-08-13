be5b41c9c0eb491744b328c41d5d2f8b4fbc4aaa
AlexandrKataev <dkeyman@mail.ru>
2022-08-14 02:47:43 +0300

    Добавление CSS-модулей

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={s.header}>
       <img src="https://www.clipartmax.com/png/full/152-1526590_emblem-of-elysium-aperture-science-logo.png"></img>
     </header>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import s from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img src="https://s0.rbk.ru/v6_top_pics/media/img/6/72/755803890510726.jpg" />
       </div>
-      <div className="avatar">
+      <div className={s.avatar}>
         <img src="https://www.hairstylenext.com/wp-content/uploads/2014/09/Tom-Cruise-Hairstyle.jpg" />
       </div>
       <div>
