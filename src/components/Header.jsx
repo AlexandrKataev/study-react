388db83e9785c48ecddab5e9ea08fc700dfe0f33
AlexandrKataev <dkeyman@mail.ru>
2022-08-13 23:38:43 +0300

    Разбивка на компоненты

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+// import "../App.css";
+
+const Header = () => {
+  return (
+    <Header className="header">
+      <img src="https://www.clipartmax.com/png/full/152-1526590_emblem-of-elysium-aperture-science-logo.png"></img>
+    </Header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+// import "./App.css";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://s0.rbk.ru/v6_top_pics/media/img/6/72/755803890510726.jpg" />
+      </div>
+      <div className="avatar">
+        <img src="https://www.hairstylenext.com/wp-content/uploads/2014/09/Tom-Cruise-Hairstyle.jpg" />
+      </div>
+      <div>
+        My posts
+        <div>New post</div>
+      </div>
+      <div>post 1</div>
+      <div>post 2</div>
+      <div>post 3</div>
+    </div>
+  );
+};
+
+export default Profile;
