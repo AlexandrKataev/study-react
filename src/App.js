cc08bf8b4705ec11de35e94272c3f1adba056f0b
AlexandrKataev <dkeyman@mail.ru>
2022-08-13 23:56:38 +0300

    После разбивки на компоненты

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Profile from "./components/Profile";
+import Header from "./components/Header.js";
+import Navbar from "./components/Navbar.js";
+import Profile from "./components/Profile.js";
 
 const App = () => {
   return (
