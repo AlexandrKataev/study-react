fc7b19631d8029b456f63642f256f8feb74d613f
AlexandrKataev <dkeyman@mail.ru>
2022-08-21 22:47:26 +0300

    components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,15 +1,6 @@
 import React from "react";
 import s from "./Dialogs.module.css";
-import { NavLink } from "react-router-dom";
-
-const DialogItem = (props) => {
-  let path = "/dialogs/" + props.id;
-  return (
-    <div className={s.dialog}>
-      <NavLink to={path}>{props.name}</NavLink>
-    </div>
-  );
-};
+import DialogItem from "./DialogItem/DialogItem";
 
 const Message = (props) => {
   return <div className={s.message}>{props.message}</div>;
