6611d12e1426c74d03598b1c75ddeee4bd249852
AlexandrKataev <dkeyman@mail.ru>
2022-08-23 20:50:31 +0300

    11

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,6 +4,13 @@ import s from "./Profile.module.css";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
+  const posts = [
+    { id: 1, message: "Hello!", likes: 5 },
+    { id: 2, message: "How are you?", likes: 13 },
+    { id: 3, message: "bye bye!", likes: 28 },
+    { id: 4, message: "Hello again!", likes: 50 },
+  ];
+
   return (
     <div className={s.content}>
       <ProfileInfo />
