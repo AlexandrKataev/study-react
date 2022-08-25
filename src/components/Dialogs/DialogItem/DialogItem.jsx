896c6e8a1236af19d20cbced28ab956a67e061b5
AlexandrKataev <dkeyman@mail.ru>
2022-08-26 02:50:44 +0300

    styles

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -6,7 +6,10 @@ const DialogItem = (props) => {
   let path = "/dialogs/" + props.id;
   return (
     <div className={s.dialog}>
-      <NavLink to={path}>{props.name}</NavLink>
+      <NavLink to={path}>
+        <img src="https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg"></img>
+        <span>{props.name}</span>
+      </NavLink>
     </div>
   );
 };
