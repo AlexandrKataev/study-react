e93676cc342500712525bde2089d28c144b13efd
AlexandrKataev <dkeyman@mail.ru>
2022-08-27 00:48:17 +0300

    верстка+лого+грид

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import s from "./Header.module.css";
+import Logo from "./Logo/Logo";
 
 const Header = () => {
   return (
     <header className={s.header}>
-      <img src="https://www.clipartmax.com/png/full/145-1453108_clipart-info-circle-vector-icon-png.png"></img>
+      <Logo></Logo>
     </header>
   );
 };
---
+++ src/components/Header/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from "./Logo.module.css";
+
+const Logo = () => {
+  return (
+    <img
+      className={s.logo}
+      src="https://vsevsevmeste.ru/wp-content/uploads/2019/09/961-9610434_png-file-svg-google-chrome-logo-png.png"
+    ></img>
+  );
+};
+
+export default Logo;
