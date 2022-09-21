c411aa8cc3b7bfdc2fde8b0825fb8656c0b873d3
AlexandrKataev <dkeyman@mail.ru>
2022-09-21 22:59:46 +0300

    get request

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,44 +1,15 @@
+import axios from "axios";
 import React from "react";
 import s from "./Users.module.css";
 
 const Users = (props) => {
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        followed: true,
-        fullName: "Kataev Alexander",
-        profileStatus: "Добрейший вечерочек",
-        location: { city: "Naberezhnie Chelny", country: "Russia" },
-        photoUrl: "https://coolsen.ru/wp-content/uploads/2021/06/0-6.jpg",
-      },
-      {
-        id: 2,
-        followed: true,
-        fullName: "Murtazin Bulat",
-        profileStatus: "Мэ",
-        location: { city: "Kazan", country: "Russia" },
-        photoUrl: "https://otkritkis.com/wp-content/uploads/2022/06/pdtrp.jpg",
-      },
-      {
-        id: 3,
-        followed: false,
-        fullName: "Kalugin Ruslan",
-        profileStatus: "Я дельфин - хочу воды",
-        location: { city: "Saint-Petesburg", country: "Russia" },
-        photoUrl:
-          "https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg",
-      },
-      {
-        id: 4,
-        followed: false,
-        fullName: "Ivanov Artem",
-        profileStatus: "Не вижу вирусов никаких",
-        location: { city: "Ostrovec", country: "Belarus" },
-        photoUrl:
-          "https://pixelbox.ru/wp-content/uploads/2021/02/mult-ava-instagram-58.jpg",
-      },
-    ]);
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        debugger;
+        props.setUsers(response.data.items);
+      });
   }
   return (
     <div>
